884ab2619ac47249a558c8d0920418e104525e2c
Higor Grigorio <higorgrigorio@gmail.com>
2023-03-20 21:25:41 -0300

    :sparkles:: Created a search bar component

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,12 +4,23 @@
     <div class="container">
         <div class="container">
             <div class="tile">
-                <div class="tile-body">
+                <div class="tile-body d-flex flex-row">
                     <x-alert />
-                    <a href="{{ url('user/new') }}" class="btn btn-warning mb-3 btn-lg">
-                        <i class="fa fa-plus-circle"></i>
-                        <span class="ml-2">Inserir novo usuário</span>
-                    </a>
+                    <div class="d-flex align-items-center col-md-10" style="padding: 0;">
+                        <form class="input-group mb-3" action="user/search" method="POST">
+                            <input type="text" class="form-control" placeholder="Search for..."
+                                aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-success" type="button">Button</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="d-flex flex-row-reverse" style="width: 100%">
+                        <a href="{{ url('user/new') }}" class="btn btn-warning mb-3 btn-md">
+                            <i class="fa fa-plus-circle"></i>
+                            <span class="ml-2 flex-nowrap">Inserir usuário</span>
+                        </a>
+                    </div>
                 </div>
                 <table class="table table-striped table-bordered table-hover">
                     <tr>
@@ -24,11 +35,11 @@
                                 <td>{{ $user->id }}</td>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
-                                <td>
-                                    <a href="{{ url('user/edit', $user->id) }}" class="btn btn-primary btn-sm">
+                                <td class="d-flex justify-content-center">
+                                    <a href="{{ url('user/edit', $user->id) }}" class="btn btn-primary btn-sm mr-2">
                                         <i class="fa fa-pencil"></i>
                                     </a>
-                                    <a href="{{ url('user/delete', $user->id) }}" class="btn btn-danger btn-sm">
+                                    <a href="{{ url('user/delete', $user->id) }}" class="btn btn-danger btn-sm ml-2">
                                         <i class="fa fa-trash"></i>
                                     </a>
                                 </td>
